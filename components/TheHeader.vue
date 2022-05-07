156ad8c275bd298e503242ceff2382642254de01
Ryan Soeder <ryan@mightily.com>
2022-05-06 22:05:01 -0400

    one stepp forward...

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -155,7 +155,7 @@ export default {
   methods: {
     internalLink(url) {
       const newRL = new URL(url)
-      if (newRL.hostname === 'tattoosalvation.com') {
+      if (newRL.hostname === 'content.tattoosalvation.com') {
         return true
       } else {
         return false
