f99ce5652015ffc79b145dcd7fa00d762364de6a
ryansoeder <ryan@mightily.com>
2023-06-30 09:38:13 -0700

    replace vue-gtag with @nuxtjs/gtm

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,152 +1,156 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Tattoo Salvation',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'The best tattoos in Louisillve, KY.',
-      },
-      {
-        name: 'keywords',
-        content:
-          'tattoo, tattoos, tattooing, louisville, kentucky, ky, traditional, realism, disposable, black, grey, sterile, color',
-      },
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: 'msapplication-TileColor', content: '#2d89ef' },
-      { name: 'theme-color', content: '#ffffff' },
-      { property: 'og:image', content: '/neon.jpg' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
-      },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-    ],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'Tattoo Salvation',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{charset: 'utf-8'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'The best tattoos in Louisillve, KY.'
+			},
+			{
+				name: 'keywords',
+				content:
+					'tattoo, tattoos, tattooing, louisville, kentucky, ky, traditional, realism, disposable, black, grey, sterile, color'
+			},
+			{name: 'format-detection', content: 'telephone=no'},
+			{name: 'msapplication-TileColor', content: '#2d89ef'},
+			{name: 'theme-color', content: '#ffffff'},
+			{property: 'og:image', content: '/neon.jpg'}
+		],
+		link: [
+			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png'
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png'
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png'
+			},
+			{rel: 'manifest', href: '/site.webmanifest'},
+			{rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'}
+		]
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/vendor/css/bootstrap.min.css',
-    '~/assets/vendor/css/bundle.min.css',
-    '~/assets/vendor/css/jquery.fancybox.min.css',
-    '~/assets/vendor/css/LineIcons.min.css',
-    '~/assets/global.css',
-  ],
-  // Transition to run on every page load
-  // pageTransition: 'fade',
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+		'~/assets/vendor/css/bootstrap.min.css',
+		'~/assets/vendor/css/bundle.min.css',
+		'~/assets/vendor/css/jquery.fancybox.min.css',
+		'~/assets/vendor/css/LineIcons.min.css',
+		'~/assets/global.css'
+	],
+	// Transition to run on every page load
+	// pageTransition: 'fade',
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/repositories.js',
-    '~/plugins/vee-validate.js',
-    '~/plugins/vue-gtag.js',
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+		'~/plugins/repositories.js',
+		'~/plugins/vee-validate.js'
+		// '~/plugins/vue-gtag.js',
+	],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: [
-    {
-      path: '~/components/',
-    },
-    {
-      path: '~/components/blocks',
-      extensions: ['vue'],
-    },
-  ],
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: [
+		{
+			path: '~/components/'
+		},
+		{
+			path: '~/components/blocks',
+			extensions: ['vue']
+		}
+	],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['@nuxtjs/google-fonts'],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: ['@nuxtjs/google-fonts'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@blokwise/dynamic',
-    'vue-sweetalert2/nuxt',
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+		'@blokwise/dynamic',
+		'vue-sweetalert2/nuxt',
+		'@nuxtjs/gtm'
+	],
 
-  gtm: {
-    id: 'G-21J8JHHXZ7',
-  },
+	gtm: {
+		id: 'GTM-NPR9SGL',
+		pageTracking: true,
+		scriptDefer: true,
+		enabled: true
+	},
 
-  sweetalert: {
-    confirmButtonColor: '#2b2b2b',
-  },
+	sweetalert: {
+		confirmButtonColor: '#2b2b2b'
+	},
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL:
-      process.env.NUXT_ENV_ENVIRONMENT === 'local'
-        ? 'http://tattoo-salvation.local/wp-json/wp/v2'
-        : 'https://content.tattoosalvation.com/wp-json/wp/v2',
-  },
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {
+		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+		baseURL:
+			process.env.NUXT_ENV_ENVIRONMENT === 'local'
+				? 'http://tattoo-salvation.local/wp-json/wp/v2'
+				: 'https://content.tattoosalvation.com/wp-json/wp/v2'
+	},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: { transpile: ['vee-validate/dist/rules'], fallback: '404.html' },
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {transpile: ['vee-validate/dist/rules'], fallback: '404.html'},
 
-  googleFonts: {
-    families: {
-      Roboto: [400, 500, 700],
-      Montserrat: [300, 400, 600, 700, 800],
-      Raleway: [400],
-    },
-  },
-  router: {
-    //https://dev.to/dimer191996/nuxt-js-smooth-scrolling-with-hash-links-94a
-    scrollBehavior: async (to, from, savedPosition) => {
-      if (savedPosition) {
-        return savedPosition
-      }
+	googleFonts: {
+		families: {
+			Roboto: [400, 500, 700],
+			Montserrat: [300, 400, 600, 700, 800],
+			Raleway: [400]
+		}
+	},
+	router: {
+		//https://dev.to/dimer191996/nuxt-js-smooth-scrolling-with-hash-links-94a
+		scrollBehavior: async (to, from, savedPosition) => {
+			if (savedPosition) {
+				return savedPosition;
+			}
 
-      const findEl = async (hash, x) => {
-        return (
-          document.querySelector(hash) ||
-          new Promise((resolve, reject) => {
-            if (x > 50) {
-              return resolve()
-            }
-            setTimeout(() => {
-              resolve(findEl(hash, ++x || 1))
-            }, 100)
-          })
-        )
-      }
+			const findEl = async (hash, x) => {
+				return (
+					document.querySelector(hash) ||
+					new Promise((resolve, reject) => {
+						if (x > 50) {
+							return resolve();
+						}
+						setTimeout(() => {
+							resolve(findEl(hash, ++x || 1));
+						}, 100);
+					})
+				);
+			};
 
-      if (to.hash) {
-        let el = await findEl(to.hash)
-        if ('scrollBehavior' in document.documentElement.style) {
-          return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
-        } else {
-          return window.scrollTo(0, el.offsetTop)
-        }
-      }
+			if (to.hash) {
+				let el = await findEl(to.hash);
+				if ('scrollBehavior' in document.documentElement.style) {
+					return window.scrollTo({top: el.offsetTop, behavior: 'smooth'});
+				} else {
+					return window.scrollTo(0, el.offsetTop);
+				}
+			}
 
-      return { x: 0, y: 0 }
-    },
-  },
+			return {x: 0, y: 0};
+		}
+	}
 
-  // target: 'static',
-}
+	// target: 'static',
+};
