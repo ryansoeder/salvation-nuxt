73b909b12c1125f6a375463524edf7fe84cf7be2
ryansoeder <ryan@mightily.com>
2023-06-29 18:20:46 -0700

    new google tag manager id

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import VueGtag from 'vue-gtag'
+import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 
 Vue.use(VueGtag, {
-  config: { id: 'G-21J8JHHXZ7' },
-})
+	config: {id: 'GTM-NPR9SGL'}
+});
